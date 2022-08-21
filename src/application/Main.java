490951e54aa69d7a632006928e68815489498823
lipollis <aline.pollis@hotmail.com>
2022-08-21 08:57:56 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,14 @@
 package application;
-
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
+
+    private static Scene mainScene;
 
     @Override
     public void start(Stage primaryStage) {
@@ -19,7 +19,7 @@ public class Main extends Application {
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
@@ -27,6 +27,11 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+
+    public static Scene getMainScene() {
+        return mainScene;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
