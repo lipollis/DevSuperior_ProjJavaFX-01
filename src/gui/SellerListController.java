5d3af9bca0acd1ef2bcd71dbaf5e4e718349eeda
lipollis <aline.pollis@hotmail.com>
2022-08-21 10:14:17 -0300

    Seller TableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,10 +1,10 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
 import application.Main;
 import db.DbIntegrityException;
 import gui.listeners.DataChangeListener;
@@ -26,55 +26,57 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
-
 public class SellerListController implements Initializable, DataChangeListener {
-
     private SellerService service;
-
     @FXML
     private TableView<Seller> tableViewSeller;
-
     @FXML
     private TableColumn<Seller, Integer> tableColumnId;
-
     @FXML
     private TableColumn<Seller, String> tableColumnName;
+
+    @FXML
+    private TableColumn<Seller, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<Seller, Date> tableColumnBirthDate;
+
+    @FXML
+    private TableColumn<Seller, Double> tableColumnBaseSalary;
 
     @FXML
     private TableColumn<Seller, Seller> tableColumnEDIT;
 
     @FXML
     private TableColumn<Seller, Seller> tableColumnREMOVE;
-
     @FXML
     private Button btNew;
-
     private ObservableList<Seller> obsList;
-
     @FXML
     public void onBtNewAction(ActionEvent event) {
         Stage parentStage = Utils.currentStage(event);
         Seller obj = new Seller();
         createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
     }
-
     public void setSellerService(SellerService service) {
         this.service = service;
     }
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializeNodes();
     }
-
     private void initializeNodes() {
         tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+        Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+        tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+        Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
     }
-
     public void updateTableView() {
         if (service == null) {
             throw new IllegalStateException("Service was null");
@@ -85,7 +87,6 @@ public class SellerListController implements Initializable, DataChangeListener {
         initEditButtons();
         initRemoveButtons();
     }
-
     private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
 //		try {
 //			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -108,17 +109,14 @@ public class SellerListController implements Initializable, DataChangeListener {
 //			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 //		}
     }
-
     @Override
     public void onDataChanged() {
         updateTableView();
     }
-
     private void initEditButtons() {
         tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
         tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
             private final Button button = new Button("edit");
-
             @Override
             protected void updateItem(Seller obj, boolean empty) {
                 super.updateItem(obj, empty);
@@ -132,12 +130,10 @@ public class SellerListController implements Initializable, DataChangeListener {
             }
         });
     }
-
     private void initRemoveButtons() {
         tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
         tableColumnREMOVE.setCellFactory(param -> new TableCell<Seller, Seller>() {
             private final Button button = new Button("remove");
-
             @Override
             protected void updateItem(Seller obj, boolean empty) {
                 super.updateItem(obj, empty);
@@ -150,10 +146,8 @@ public class SellerListController implements Initializable, DataChangeListener {
             }
         });
     }
-
     private void removeEntity(Seller obj) {
         Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
-
         if (result.get() == ButtonType.OK) {
             if (service == null) {
                 throw new IllegalStateException("Service was null");
